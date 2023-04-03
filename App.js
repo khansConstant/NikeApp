e2bae720821626e6a73ea04c8419059873b22bf9
khansConstant <huzaifakahn.98@gamil.com>
2023-04-03 17:24:11 +0530

    Products data

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import ProductScreen from './src/screens/ProductScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <ProductScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+
+  },
+
+});
+
